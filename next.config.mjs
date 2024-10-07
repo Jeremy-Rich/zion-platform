d333d7ea6b4b2ac3272c37a3c298f089ecaffd57
JB <87101383+Jeremy-Rich@users.noreply.github.com>
2024-10-07 18:31:59 +1100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,46 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    domains: ['example.com'],
+  },
+  env: {
+    CUSTOM_KEY: 'my-value',
+  },
+  i18n: {
+    locales: ['en-US', 'fr', 'nl'],
+    defaultLocale: 'en-US',
+  },
+  webpack(config, { isServer }) {
+    // Further custom configuration here
+    return config;
+  },
+  async redirects() {
+    return [
+      {
+        source: '/old-path',
+        destination: '/new-path',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
